607b49d36d8d2b5f75b2c028e53d83c33d80a8f8
Ashra <waseka@bitmascot.com>
2021-04-24 14:15:08 +0600

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException {
+        SeleniumTest selenium = new SeleniumTest();
+        selenium.test();
+    }
+}
